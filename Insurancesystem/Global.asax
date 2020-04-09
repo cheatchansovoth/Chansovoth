8b517917d9626455eda538662fcfba8910525775
Chansovoth <cheatchansovoth@gmail.com>
2020-04-10 03:21:43 +1200

    Insurance
    Insurance

---
+++ Insurancesystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Insurancesystem.MvcApplication" Language="C#" %>
---
+++ Insurancesystem/Scripts/jQuery.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+     $(".claimform").hide();
+     $("select").change(function ()
+    {
+        if (this.value == "0") {
+            $(".claimform").hide();
+        }
+        else if (this.value == "1")
+        {
+            $(".claimform").show();
+        }
+
+        else
+        {
+            $(".claimform").show();    
+        }
+    });
+});
+$(".dropdown").hover(function () {
+    //toggle the open class (on/off)
+    $(this).toggleClass("open");
+});
