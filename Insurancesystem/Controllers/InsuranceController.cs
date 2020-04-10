a3b17522cdb6548c1b5a5ab024128b1dcdc5ec9a
Chansovoth <cheatchansovoth@gmail.com>
2020-04-11 03:19:31 +1200

    Update

--- Insurancesystem/Controllers/InsuranceController.cs
+++ Insurancesystem/Controllers/InsuranceController.cs
@@ -9,7 +9,7 @@ namespace Insurancesystem.Controllers
 {
     public class InsuranceController : Controller
     {
-        InsuranceDBEntities3 db = new InsuranceDBEntities3();
+        InsuranceDBEntities5 db = new InsuranceDBEntities5();
         // GET: Insurance
         public ActionResult Index()
         {
@@ -17,15 +17,26 @@ namespace Insurancesystem.Controllers
         }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(CusContact cus)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CusContacts.Add(cus);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Testing()
+        {
             return View();
         }
         public ActionResult carinformation()
@@ -40,81 +51,11 @@ namespace Insurancesystem.Controllers
         {
             return View();
         }
-        public ActionResult onCreate()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult onCreate(InsuranceUser user)
-        {
-                if (ModelState.IsValid)
-                {
-                    db.InsuranceUsers.Add(user);
-                    db.SaveChanges();
-                }
-                return RedirectToAction("Index");
-
-        }
-        public ActionResult createCar()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult createCar(Car car)
-        {
-                if (ModelState.IsValid)
-                {
-                    db.Cars.Add(car);
-                    db.SaveChanges();
-                }
-                return RedirectToAction("OnCreate");
-        }
-        public ActionResult createMotor()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult createMotor(MotorBike motor)
-        {
-                if (ModelState.IsValid)
-                {
-                    db.MotorBikes.Add(motor);
-                    db.SaveChanges();
-                }
-                return RedirectToAction("OnCreate");
-        }
-        public ActionResult Claimform()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Claimform(Claim claim)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Claims.Add(claim);
-                db.SaveChanges();
-            }
-            return RedirectToAction("Index");
-        }
+        
         public ActionResult Login()
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult Login(InsuranceUser user)
-        {
-            int sx = db.InsuranceUsers.Where(x => x.Username == user.Username).Count();
-            if (sx == 0)
-            {
-                ViewBag.msg = "Check your username and password";
-                return RedirectToAction("Login", user);
-            }
-            else
-            {
-                var User = db.InsuranceUsers.Where(x => x.Username == user.Username && x.Password == user.Password).FirstOrDefault();
-                return RedirectToAction("Index");
-            }
-        }
+        
      }
  }
